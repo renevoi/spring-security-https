4a7450422fb677015a02c6186116ad30162e8f16
rene <rene.catapang@gmail.com>
2019-01-11 13:46:35 +0800

    Initial commit

---
+++ src/main/java/com/renevoi/springsecurityhttps/SpringSecurityHttpsApplication.java
@@ -0,0 +1,12 @@
+package com.renevoi.springsecurityhttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHttpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityHttpsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/renevoi/springsecurityhttps/SpringSecurityHttpsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.renevoi.springsecurityhttps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityHttpsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
